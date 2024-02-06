d64e8c9eb65ee48b90e51ab129005ec2060ea56f
root <root@Ayush.Ayush>
2024-02-06 13:01:59 -0500

    Basic program of Java

---
+++ avg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class avg {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String name = sc.nextLine();
+        System.out.println("Your name is :"+name);
+    }
+
+    
+}
---
+++ demo.java
@@ -0,0 +1,5 @@
+public class demo{
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+    }
+}
